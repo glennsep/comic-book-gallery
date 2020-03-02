7f0a92430a184c8579a5f13dba812d774dc8b7e1
glennsep <61672308+glennsep@users.noreply.github.com>
2020-03-01 21:10:23 -0500

    Add files via upload

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ComicBookGallery.Models;
+using ComicBookGallery.Data;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+            return View(comicBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
+
+            return View(comicBook);
+        }
+    }
+}
